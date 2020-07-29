1c628689ca8d70da4611f52e1bf4a49dab1639cd
menycv <menycalzadil@hotmail.com>
2020-07-28 21:08:11 -0600

    Avance en menu principal

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Interfaz(Tk):
         super().__init__()  # Se inicia la ventana
         self.myFont = ("Verdana", 12)
         self.color = "#2ECC71"
+        self.sesion = ""
         # Usernames
         self.usernames = ("Admin", "Service", "Operador 1", "Operador 2",
                     "Operador 3")
@@ -38,8 +39,8 @@ class Interfaz(Tk):
         #                       self.winfo_screenheight()))
         self.geometry("800x400")
 
-        Home(self).tkraise()
-        # Inicio(self).tkraise()
+        # Home(self).tkraise()
+        Inicio(self).tkraise()
 # -----------------------------------------------------------------------------
 
 # ---------------------- Ventana de inicialización ----------------------------
@@ -328,7 +329,8 @@ class Teclado(Frame):
 
     def ver_pass(self):
         if self.root.passwords[self.title] == self.pass_try:
-            self.espacio.config(bg=self.root.color)
+            self.espacio.config(bg="blue")
+            self.root.sesion = self.title
             self.after(500, self.acceso)
         else:
             self.espacio.config(bg="red")
@@ -341,14 +343,120 @@ class Teclado(Frame):
 
     def acceso(self):
         self.grid_forget()
-        Menu_principal(self.root).tkraise()
+        Menu_principal(self.root, "Principal").tkraise()
 # ---------------- Termina ventana de teclado numérico ------------------------
 
 class Menu_principal(Frame):
-    def __init__(self, root):
+    def __init__(self, root, menu):
         super().__init__(root)
+        # Variables
+        self.root = root
+        self.mensaje = ""
+        self.current_state = 0
+        self.current_menu = "Principal"
+        self.timer = 0
+        self.state = ("Normal", "Step", "Testing", "Manual")
+        self.funcs = ["START/STOP", "CONF", "DIAG", "LOG"]
+        self.menu_buttons = []
+        # Metodos
         self.config(bg="white")
         self.grid(column=0, row=0)
-        label = Label(self, bg="white", text="Menu").pack()
+
+        self.create_widgets(self.current_menu)
+
+    def create_widgets(self, menu):
+        if menu == "Principal":
+            self.menu = Frame(self, bg="white", bd=2, relief=SOLID)
+            self.menu.grid(column=0, row=0)
+
+            for func in self.funcs:
+                self.menu_button_principal(func)
+
+            self.main_container = Frame(self, bg="white")
+            self.main_container.grid(column=0, row=1)
+            # Imagenes del boton de encendido
+            self.start_button_pause = PhotoImage(
+                file="images//off.png").subsample(6)
+            self.start_button_active = PhotoImage(
+                file="images//on.png").subsample(6)
+
+            self.start_button_frame = Frame(self.main_container, bg="white")
+            self.start_button_frame.pack(side=LEFT, pady=(10,10))
+
+            if self.current_state == 0:
+                self.start_button = Label(self.start_button_frame, bg="white",
+                    image=self.start_button_pause)
+                self.start_button.bind("<Button-1>", self.activate)
+                self.start_button.pack(side=TOP)
+                self.start_button_label = Label(self.start_button_frame,
+                    bg="white", text=self.state[0], font=self.root.myFont)
+                self.start_button_label.pack(side=BOTTOM)
+            else:
+                self.start_button.config(image=self.start_button_active)
+
+            # Frame para las imagenes de los menus
+            self.visual_menus_frame = Frame(self.main_container, bg="white")
+            self.visual_menus_frame.pack(side=RIGHT)
+
+            self.conf_icon = PhotoImage(file="images//conf.png")
+            self.diag_icon = PhotoImage(file="images//diag.png").subsample(6)
+            self.log_icon = PhotoImage(file="images//log.png").subsample(6)
+
+            self.visual_menus_list = (
+                (self.funcs[1], self.conf_icon, self.conf_menu),
+                (self.funcs[2], self.diag_icon, self.diag_menu),
+                (self.funcs[3], self.log_icon, self.log_menu)
+            )
+
+            for text, image, func in self.visual_menus_list:
+                self.visual_menus(text, image, func)
+
+        elif menu == "Config":
+            pass
+        elif menu == "DIAG":
+            pass
+        elif menu == "LOG":
+            pass
+
+    def conf_menu(self):
+        pass
+
+    def diag_menu(self):
+        pass
+
+    def log_menu(self):
+        pass
+
+    def visual_menus(self, text, image, func):
+        frame_button = Frame(self.visual_menus_frame, bg="white")
+        frame_button.pack(side=TOP)
+        button = Label(frame_button, bg="white",
+            image=image)
+        button.bind("<Button-1>", func)
+        button.pack(side=TOP)
+        button_label = Label(frame_button, bg="white",
+            text=text)
+        button_label.pack(side=BOTTOM)
+
+    def activate(self, event):
+        if self.current_state == 0:
+            self.current_state = 1
+        else:
+            self.current_state = 0
+
+        self.create_widgets(self.current_menu)
+
+    def menu_button_principal(self, text):
+        self.menu_buttons.append(Button(self.menu, fg="white", width=19,
+            bg=self.root.color, font=self.root.myFont, text=text).pack(
+            fill=BOTH, side=LEFT, expand=YES))
+
+    def menu_button(self, text):
+        pass
+
+    def cambiar_sesion(self):
+        self.grid_forget()
+        self.root.sesion = ""
+        Inicio(self.root).tkraise()
 
 Interfaz().mainloop()
