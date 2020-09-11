667faa0e9848f697a9fbed5c7149ddf33289b7d8
menycv <menycalzadil@hotmail.com>
2020-09-10 21:04:54 -0600

    Se agrega función de opciones de programa

--- program_menu.py
+++ program_menu.py
@@ -11,7 +11,7 @@ class Program_menu():
         self.main = Frame(self.root_frame, bg="white")
         self.main.grid(sticky=W, column=0, row=1)
 
-        self.frame_programa = Frame(self.main, bg="white")
+        self.frame_programa = Frame(self.main, bg="white", bd=3, relief=RIDGE)
         self.frame_programa.grid(column=0, row=0, sticky=W, padx=20, pady=10)
 
         self.frame_titulo_programa = Frame(self.frame_programa, bg="white",
@@ -20,4 +20,13 @@ class Program_menu():
         self.titulo = Label(self.frame_titulo_programa, bg="white",
             text="Programa", font=("Verdana", 15, "bold"))
         self.titulo.pack(padx=30, pady=5)
-        
+        self.opcion("NORMAL", 1)
+        self.opcion("MANUAL", 2)
+
+    def opcion(self, prog, row):
+        fr = Frame(self.frame_programa, bg="white", bd=3, relief=RIDGE)
+        fr.grid(column=0, row=row, sticky=W, pady=5)
+        ch_button = Checkbutton(fr, bg="white")
+        ch_button.pack(side=LEFT)
+        program_lbl = Label(fr, text=prog, bg="white", font=self.root.myFont)
+        program_lbl.pack(side=LEFT)
