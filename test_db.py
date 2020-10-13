ee658f24c6e7d5611f0814cfad17e8898f9a87cb
Calzadillas <menycalzadil@hotmail.com>
2020-10-13 09:37:12 -0600

    Se agrega log file

--- test_db.py
+++ test_db.py
@@ -3,9 +3,15 @@ import sqlite3
 database = sqlite3.connect("program_database.db")
 print("connection succsessfull")
 
-database.execute("INSERT INTO spanish_phrases(ID, PHRASE) VALUES (1, 'BIENVENIDO');")
+cmd = input("Introduzca comando sql\n")
+a = 0
+
+while a != 1:
+    datos = database.execute(cmd)
+    for dato in datos:
+        print(dato)
+    a = int(input("Si desea continuar escriba 0, de lo contrario 1\n"))
 database.commit()
 database.close()
 
 print("Insertion successfull")
-
