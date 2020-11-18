c6a03bb07adcfb5c1d6bb98cf6c2c890c5074ace
menycv <menycalzadil@hotmail.com>
2020-11-17 18:22:37 -0700

    Se actualiza el menú de programas para que funcione con las nuevas variables

--- programas.py
+++ programas.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from datetime import date, datetime
 import log_menu
 import sqlite3
@@ -12,13 +12,8 @@ class Programas():
 		self.program = programa
 		self.root.fecha_inicio = date.today()
 		self.root.hora_inicio = datetime.now().strftime("%H:%M:%S")
-
 		if self.program == "NORMAL":
 			self.normal()
-		elif self.program == "STEP":
-			self.step()
-		elif self.program == "TESTING":
-			self.testing()
 		elif self.program == "MANUAL":
 			self.manual()
 
@@ -26,20 +21,20 @@ class Programas():
 		self.previous_frame = previous_frame
 
 	def normal(self):
-			self.inicio = 0
-			self.root.ml = 0
-			self.tiempo_salida = 5
-			self.root.temp_dht_inicial = self.root.temp_dht
-			self.root.humidity_dht_inicial = self.root.humidity_dht
-			self.root.color_alertas = "orange"
-			self.previous_frame.alertas_frame.config(bg=self.root.color_alertas)
-			self.previous_frame.alertas_label.config(bg=self.root.color_alertas)
-			self.root.mensaje = "STATUS: Comenzando a operar salga de la habitación en " + str(self.tiempo_salida) + " seg"
-			self.previous_frame.alertas_label.config(text=self.root.mensaje)
-			if self.root.concentracion != 0 and self.root.vol != 0:
-				self.total_ml = self.root.concentracion * self.root.vol
-				self.tiempo_a_sanitizar = self.total_ml * .1
-				self.root.after(1000, self.salida_start)
+		self.inicio = 0
+		self.root.ml = 0
+		self.tiempo_salida = 5
+		self.root.temp_dht_inicial = self.root.temp_dht
+		self.root.humidity_dht_inicial = self.root.humidity_dht
+		self.root.color_alertas = "orange"
+		self.previous_frame.alertas_frame.config(bg=self.root.color_alertas)
+		self.previous_frame.alertas_label.config(bg=self.root.color_alertas)
+		self.root.mensaje = "STATUS: Comenzando a operar salga de la habitación en " + str(self.tiempo_salida) + " seg"
+		self.previous_frame.alertas_label.config(text=self.root.mensaje)
+		if self.root.concentracion != 0 and self.root.vol != 0:
+			self.total_ml = self.root.concentracion * self.root.vol
+			self.tiempo_a_sanitizar = self.total_ml * .1
+			self.root.after(1000, self.salida_start)
 
 	def salida_start(self):
 		self.tiempo_salida = self.tiempo_salida - 1
@@ -53,7 +48,8 @@ class Programas():
 					self.previous_frame.alertas_frame.config(bg=self.root.color_alertas)
 					self.previous_frame.alertas_label.config(text=self.root.mensaje,
 						bg=self.root.color_alertas)
-				self.root.pin_on(self.root.bomba_entrada, 0)
+				self.root.be[1] = 0
+				self.root.pin_on(self.root.be[0], self.root.be[1])
 				self.root.after(100, self.measure_ml)
 			else:
 				self.root.after(1000, self.salida_start)
@@ -65,23 +61,25 @@ class Programas():
 					bg=self.root.color)
 			self.root.program_object = None
 
-	def step(self):
-		pass
-
-	def testing(self):
-		pass
-
 	def manual(self):
-		self.root.pin_on(self.root.bomba_entrada, 0)
+		self.root.be[1] = 0
+		self.root.s1[1] = 0
+		self.root.s2[1] = 0
+		self.root.be[1] = 0
+		self.root.pin_on(self.root.be[0], self.root.be[1])
 		if GPIO.input(self.root.flotador) == 0:
-			self.root.pin_on(self.root.bomba_entrada, 1)
+			self.root.be[1] = 1
+			self.root.pin_on(self.root.be[0], self.root.be[1])
 
 
 	def measure_ml(self):
             if self.root.current_button_state == 1:
                 # flujo en ml/s
-                # self.root.flujo = ((self.root.pulsos/0.1)/70)*(1000/60)
-                self.root.flujo = 30
+                if self.root.mode == "rasp":
+                    # self.root.flujo = ((self.root.pulsos/0.1)/70)*(1000/60)
+                    self.root.flujo = 30
+                else:
+                    self.root.flujo = 30
                 self.root.pulsos = 0
                 self.root.ml = self.root.ml + (self.root.flujo * .1)
                 self.root.mensaje = "STATUS: LLENANDO; " + str(int(self.root.ml)) + \
@@ -92,10 +90,11 @@ class Programas():
                     self.previous_frame.alertas_label.config(bg="blue")
                     self.previous_frame.alertas_frame.config(bg="blue")
 
-                # if GPIO.input(self.root.flotador) == 0:
-                #     self.root.pin_on(self.root.bomba_entrada, 1)
-                #     self.root.after(120000, self.llenar_tanque)
-                #     self.root.tanque_lleno = 1
+                if GPIO.input(self.root.flotador) == 0:
+                    self.root.be[1] = 1
+                    self.root.pin_on(self.root.be[0], self.root.be[1])
+                    self.root.after(120000, self.llenar_tanque)
+                    self.root.tanque_lleno = 1
                 if self.root.ml >= 320 and self.inicio == 0:
                     self.inicio = 1
                     self.root.ml = 0
@@ -103,7 +102,8 @@ class Programas():
                     self.root.tanque_lleno = 1
                 elif self.root.ml >= self.total_ml and self.inicio == 1:
                     # Fin de programa
-                    self.root.pin_on(self.root.bomba_entrada, 1)
+                    self.root.be[1] = 1
+                    self.root.pin_on(self.root.be[0], self.root.be[1])
                     self.root.color_alertas = "orange"
                     self.root.mensaje = "STATUS: SANITIZANDO "
                     if self.root_frame.current_menu == "START/STOP":
@@ -115,7 +115,8 @@ class Programas():
             else:
                 self.root.pulses = 0
                 self.root.ml = 0
-                self.root.pin_on(self.root.bomba_entrada, 1)
+                self.root.be[1] = 1
+                self.root.pin_on(self.root.be[0], self.root.be[1])
                 if self.root.tanque_lleno == 1:
                     self.vaciar_tanque()
                 else:
@@ -134,7 +135,8 @@ class Programas():
 			self.previous_frame.alertas_frame.config(bg=self.root.color_alertas)
 			self.previous_frame.alertas_label.config(
 				text=self.root.mensaje, bg="orange")
-		self.root.pin_on(self.root.bomba_salida, 0)
+		self.root.bs[1] = 0
+		self.root.pin_on(self.root.root.bs[0], self.root.bs[1])
 		self.root.after(80000, self.apagar_bomba)
 
 	def terminar_normal(self):
@@ -161,13 +163,15 @@ class Programas():
 			self.vaciar_tanque()
 
 	def llenar_tanque(self):
-                self.root.pin_on(self.root.bomba_entrada, 0)
+                self.root.be[1] = 0
+                self.root.pin_on(self.root.be[0], self.root.be[1])
                 self.root.after(100, self.measure_ml)
 
 	def apagar_bomba(self):
 		self.root.color_alertas = self.root.color
 		self.root.tanque_lleno = 0
-		self.root.pin_on(self.root.bomba_salida, 1)
+		self.root.bs[1] = 1
+		self.root.pin_on(self.root.bs[0], self.root.bs[1])
 		self.root.program_object = None
 		self.root.current_button_state = 0
 		self.root.mensaje = "STATUS: LISTO PARA OPERAR"
